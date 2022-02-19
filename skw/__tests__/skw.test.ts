bd0cc5466087cf7120cdceae222d7b44b7fecdd9
Kenta Kase <kesin1202000@gmail.com>
2022-02-19 22:44:22 +0900

    Add tests

--- skw/__tests__/skw.test.ts
+++ skw/__tests__/skw.test.ts
@@ -1,5 +1,13 @@
-import {describe, expect, test} from '@jest/globals'
-import {createTagOptions} from '../src/skw'
+import {describe, expect, test, jest, afterEach} from '@jest/globals'
+import {createTagOptions, upload, download} from '../src/skw'
+import {ActionsInputs} from '../src/inputs'
+import exec from "@actions/exec"
+
+jest.mock("@actions/exec", () => {
+  return {
+    exec: jest.fn()
+  }
+})
 
 describe('createTagOptions', () => {
   test('single tag', async () => {
@@ -10,6 +18,101 @@ describe('createTagOptions', () => {
   test('multiple tags', async () => {
     const actual = createTagOptions(["latest", "beta"])
     expect(actual).toEqual(["-t", "latest", "-t", "beta"])
+  })
+})
+
+describe('upload', () => {
+  const skwPath = "/tmp/skw.jar"
+  const baseInputs: ActionsInputs = {
+    version: "1.0.0",
+    command: "upload",
+    bucket: "gs://test",
+    key: "test",
+    tags: ["latest", "beta"],
+    prefix: "prefix",
+    paths: ["test.js", "test.ts"],
+  }
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  test('Input all parameters', async () => {
+    const inputs = baseInputs
+    const spy = jest.spyOn(exec, "exec")
+
+    await upload(skwPath, inputs)
+    expect(spy).lastCalledWith("java", [
+      "-jar",
+      skwPath, inputs.command,
+      "-b", inputs.bucket,
+      "-k", inputs.key,
+      "-t", inputs.tags[0], "-t", inputs.tags[1],
+      "-p", inputs.prefix,
+      inputs.paths[0], inputs.paths[1]
+    ])
+  })
+
+  test('Input without prefix', async () => {
+    const inputs = { ...baseInputs, prefix: undefined }
+    const spy = jest.spyOn(exec, "exec")
+
+    await upload(skwPath, inputs)
+    expect(spy).lastCalledWith("java", [
+      "-jar",
+      skwPath, inputs.command,
+      "-b", inputs.bucket,
+      "-k", inputs.key,
+      "-t", inputs.tags[0], "-t", inputs.tags[1],
+      inputs.paths[0], inputs.paths[1]
+    ])
+  })
+})
+
+describe('download', () => {
+  const skwPath = "/tmp/skw.jar"
+  const baseInputs: ActionsInputs = {
+    version: "1.0.0",
+    command: "download",
+    bucket: "gs://test",
+    key: "test",
+    tags: ["latest"],
+    paths: ["downloadDir"],
+  }
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  test('Input all parameters', async () => {
+    const inputs = baseInputs
+    const spy = jest.spyOn(exec, "exec")
+
+    await download(skwPath, inputs)
+    expect(spy).lastCalledWith("java", [
+      "-jar",
+      skwPath, inputs.command,
+      "-b", inputs.bucket,
+      "-k", inputs.key,
+      "-t", inputs.tags[0],
+      inputs.paths[0]
+    ])
+  })
+
+  test('Input with redundant parameters', async () => {
+    const inputs = { ...baseInputs,
+      tags: ["latest", "beta"],
+      paths: ["downloadDir1", "downloadDir2"]
+    }
+    const spy = jest.spyOn(exec, "exec")
+
+    await download(skwPath, inputs)
+    expect(spy).lastCalledWith("java", [
+      "-jar",
+      skwPath, inputs.command,
+      "-b", inputs.bucket,
+      "-k", inputs.key,
+      "-t", inputs.tags[0],
+      inputs.paths[0]
+    ])
   })
 })
 
